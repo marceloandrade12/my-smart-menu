9622c2505408d1a5aa1e3868656d1ec818159230
Marcelo Andrade <andrademarcelo98@gmail.com>
2024-05-03 19:34:28 +0100

    feat: added home page content

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,5 +1,27 @@
+import { Flex, Stack, Text } from "@chakra-ui/react";
 import React from "react";
+import { useTranslation } from "react-i18next";
+import imgUrl from "../../assets/logo.png";
 
 export const Home: React.FC = () => {
-  return <React.Fragment>Home</React.Fragment>;
+  const { t } = useTranslation();
+  return (
+    <React.Fragment>
+      <Stack
+        w="70%"
+        alignItems="center"
+        flexGrow="1"
+        spacing={2}
+        textAlign="center"
+        justifyContent="space-evenly"
+      >
+        <Flex w="50%">
+          <img src={imgUrl} alt="My Smart Menu Logo" />
+        </Flex>
+        <Text fontSize="3xl">{t("home.title")}</Text>
+        <Text fontSize="lg">{t("home.text")}</Text>
+        <Text color="blueviolet">{t("home.tryNow")}</Text>
+      </Stack>
+    </React.Fragment>
+  );
 };
